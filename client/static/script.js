ddad82293eec79157847a31a569bf1e559ac4a8b
Corneliu Laur <cl179971@gmail.com>
2021-08-24 18:46:27 +0100

    amendments to html, css and script.js

--- client/static/script.js
+++ client/static/script.js
@@ -1,12 +1,19 @@
 // Add event listener to form
+const form = document.querySelector("form");
+const btn = document.querySelector("#btn");
 
-// create function to retrieve the data from the form
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const data = Object.fromEntries(new FormData(e.target));
+    const date = new Date();
+    const dateString = `${date.getFullYear()}${date.getMonth()}${date.getDay()}`;
+    data.dateString = `${data.title}-${dateString}`;
+    console.log(data);
+});
 
 // validate the user input
 
-// make a post request onclick listener -- btn
 // redirect user to a new url/window
 
-
 // upon redirection create a fetch request for the data/post
-
