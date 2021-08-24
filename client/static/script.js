c8d0a06a32fe1d484ad72b9fb02ec0024885f1e2
Corneliu Laur <cl179971@gmail.com>
2021-08-24 17:17:02 +0100

    created static folder with script.js

---
+++ client/static/script.js
@@ -0,0 +1,12 @@
+// Add event listener to form
+
+// create function to retrieve the data from the form
+
+// validate the user input
+
+// make a post request onclick listener -- btn
+// redirect user to a new url/window
+
+
+// upon redirection create a fetch request for the data/post
+
