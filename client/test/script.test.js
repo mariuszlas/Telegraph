87f41cc67b67bd380e4736d2c9140a08537e7894
Corneliu Laur <cl179971@gmail.com>
2021-08-25 11:19:44 +0100

    tests for script.js

--- client/test/script.test.js
+++ client/test/script.test.js
@@ -1,1 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+const { hasUncaughtExceptionCaptureCallback } = require("process");
+const html = fs.readFileSync(path.resolve(__dirname, "../index.html"));
+document.documentElement.innerHTML = html.toString();
+const script = require("../static/script");
 
+describe("displayPost", () => {
+    test("check for number of sections", () => {
+        const sections = document.querySelectorAll("section");
+
+        expect(sections.length).toBe(3);
+    });
+
+    test("number of paragraphs within postSection", () => {
+        const paragraphs = document.querySelectorAll("p.postField");
+
+        expect(paragraphs.length).toBe(5);
+    });
+});
